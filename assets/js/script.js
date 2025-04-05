0802dc2fb68bd1fcc95ae9d0bfd8bf6220db524b
Kc-Vic <6a9a218x@students.codeinstitute.net>
2025-04-05 18:13:31 +0100

    Add code to check the answer and respond to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,27 +4,55 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
     }
 });
 
-function runGame() {
+function runGame(gameType) {
         // Creates two random numbers between 1 and 25
         let num1 = Math.floor(Math.random() * 25) + 1;
         let num2 = Math.floor(Math.random() * 25) + 1;
+
+        if (gameType === "addition") {
+            displayAdditionQuestion(num1, num2);
+        } else {
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknown game type: ${gameType}. Aborting!`;
+        }
 }
 
 function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById('answer-box').value);
+    let correctAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === correctAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D")
+        incrementScore();
+    } else {
+        alert(`Awwww.... you answered ${userAnswer}. The correct answer was ${correctAnswer[0]}!`);
+    }
+
+    runGame(correctAnswer[1]);
 
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById('operator').innerText;
 
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unknown operator: ${operator}`);
+        throw `Unknown operator: ${operator}. Aborting!`;
+    }
 }
 
 function incrementScore() {
@@ -35,8 +63,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = '+';
 }
 
 function displaySubtractQuestion() {
